77b62c7a3765047a9191dc3f71574906043f58ca
root <root@LAPTOP-2FU2GSHL.localdomain>
2019-02-17 11:28:04 +0530

    update

---
+++ chap2exe.py
@@ -0,0 +1,23 @@
+print('1\n')
+nam = raw_input("enter your name:")
+print("\nhello"+nam)
+print('\n 2 \n')
+hours = input('enter hours:\n')
+rate = input('enter rate: \n')
+pay = hours*rate
+print('pay:\t',pay,'\n')
+print('3 \n')
+width = 17
+height = 12.0
+print ('width = \t', width)
+print('\n height = \t', height)
+print('\nwidth//2: \t', width//2)
+print('\nwidth/2.0: \t' , width/2.0)
+print('\n height/3: \t', height/3)
+su = 1+2*5
+print('\n 1+2*5: \t',su)
+print('\n 4 \n')
+cel= input('enter celsius : \t')
+fa = (cel*9/5)+32
+print('\n Fahrenheit:\t',fa)
+
